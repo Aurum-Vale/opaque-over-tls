5cf9dbd56c4d7ac4fd49bbb90311b5d524936703
Aurélien AUZEMERY <aurelien.auzemery@gmail.com>
2023-01-22 15:37:05 +0100

    Basic TCP connection (from the Rust book)

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,3 +1,17 @@
+use std::{
+    io::{Read, Write},
+    net::TcpStream,
+};
+
 fn main() {
-    println!("Hello, world! (Client)");
+    let server_ip = "127.0.0.1:7878";
+    let mut stream = TcpStream::connect(server_ip).unwrap();
+
+    let req = "\r\n\r\n";
+    stream.write_all(req.as_bytes()).unwrap();
+
+    let mut res = String::new();
+    stream.read_to_string(&mut res).unwrap();
+
+    println!("{res}");
 }
