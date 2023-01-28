554c516631407a46275712cf7a26084628d86861
Aurélien AUZEMERY <aurelien.auzemery@gmail.com>
2023-01-28 23:19:02 +0100

    Minimal TLS connection example

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,17 +1,98 @@
 use std::{
-    io::{Read, Write},
-    net::TcpStream,
+    fs,
+    io::{BufReader, Read, Write},
+    net::{IpAddr, Ipv4Addr, TcpStream},
+    sync::Arc,
 };
 
+use rustls::ClientConnection;
+
 fn main() {
+    // Create the CA root store, add the CA certificate to it
+    let mut root_store = rustls::RootCertStore::empty();
+
+    let root_crt_file = fs::File::open("openssl/rootCA.crt").unwrap();
+    let mut br = BufReader::new(root_crt_file);
+
+    let cert_u8 = rustls_pemfile::certs(&mut br).unwrap().pop().unwrap();
+
+    let cert = rustls::Certificate(cert_u8);
+
+    root_store.add(&cert).unwrap();
+
+    println!("{}", root_store.len());
+
+    let config = Arc::new(
+        rustls::ClientConfig::builder()
+            .with_safe_defaults()
+            .with_root_certificates(root_store)
+            .with_no_client_auth(),
+    );
+
+    let server_name = rustls::ServerName::try_from("test.localhost").expect("invalid DNS name");
+
+    let mut tls_conn = ClientConnection::new(config, server_name).unwrap();
+
     let server_ip = "127.0.0.1:7878";
-    let mut stream = TcpStream::connect(server_ip).unwrap();
+    let mut socket = TcpStream::connect(server_ip).unwrap();
 
-    let req = "\r\n\r\n";
-    stream.write_all(req.as_bytes()).unwrap();
+    let mut stream = rustls::Stream::new(&mut tls_conn, &mut socket);
 
-    let mut res = String::new();
-    stream.read_to_string(&mut res).unwrap();
+    if stream.conn.is_handshaking() {
+        println!("Handshaking in progress.");
+    }
 
+    if stream.conn.wants_write() {
+        println!("Wants writing.");
+    }
+
+    if stream.conn.wants_read() {
+        println!("Wants reading.");
+    }
+
+    match stream.conn.complete_io(&mut stream.sock) {
+        Ok(res) => {
+            println!("Handshake done.");
+            println!("{res:?}");
+        }
+        Err(err) => {
+            println!("{err:#?}");
+            return;
+        }
+    };
+
+    if stream.conn.is_handshaking() {
+        println!("Handshaking in progress.");
+    }
+
+    if stream.conn.wants_write() {
+        println!("Wants writing.");
+    }
+
+    if stream.conn.wants_read() {
+        println!("Wants reading.");
+    }
+
+    let res = stream.write(&[42, 0]).unwrap();
     println!("{res}");
+
+    if stream.conn.is_handshaking() {
+        println!("Handshaking in progress.");
+    }
+
+    if stream.conn.wants_write() {
+        println!("Wants writing.");
+    }
+
+    if stream.conn.wants_read() {
+        println!("Wants reading.");
+    }
+
+    // let req = "\r\n\r\n";
+    // socket.write_all(req.as_bytes()).unwrap();
+
+    // let mut res = String::new();
+    // socket.read_to_string(&mut res).unwrap();
+
+    // println!("{res}");
 }
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,17 +1,101 @@
 use std::{
+    fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
+    sync::Arc,
 };
 
+use rustls::ServerConnection;
+
+pub fn load_certs(filename: &str) -> Vec<rustls::Certificate> {
+    let certfile = fs::File::open(filename).expect("cannot open certificate file");
+    let mut reader = BufReader::new(certfile);
+    rustls_pemfile::certs(&mut reader)
+        .unwrap()
+        .iter()
+        .map(|v| rustls::Certificate(v.clone()))
+        .collect()
+}
+
+fn load_private_key(filename: &str) -> rustls::PrivateKey {
+    let keyfile = fs::File::open(filename).expect("cannot open private key file");
+    let mut reader = BufReader::new(keyfile);
+
+    loop {
+        match rustls_pemfile::read_one(&mut reader).expect("cannot parse private key .pem file") {
+            Some(rustls_pemfile::Item::RSAKey(key)) => return rustls::PrivateKey(key),
+            Some(rustls_pemfile::Item::PKCS8Key(key)) => return rustls::PrivateKey(key),
+            Some(rustls_pemfile::Item::ECKey(key)) => return rustls::PrivateKey(key),
+            None => break,
+            _ => {}
+        }
+    }
+
+    panic!(
+        "no keys found in {:?} (encrypted keys not supported)",
+        filename
+    );
+}
+
 fn main() {
+    let certs = load_certs("openssl/server.crt");
+    let private_key = load_private_key("openssl/server.key");
+
+    let config = Arc::new(
+        rustls::ServerConfig::builder()
+            .with_safe_defaults()
+            .with_no_client_auth()
+            .with_single_cert(certs, private_key)
+            .expect("bad certificates/private key"),
+    );
+
+    let mut tls_conn = ServerConnection::new(config).unwrap();
+
     let server_ip = "127.0.0.1:7878";
     let listener = TcpListener::bind(server_ip).unwrap();
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
+    let mut socket = listener.accept().unwrap().0;
+    let mut stream = rustls::Stream::new(&mut tls_conn, &mut socket);
 
-        handle_connection(stream);
+    if stream.conn.is_handshaking() {
+        println!("Handshaking in progress.");
     }
+
+    if stream.conn.wants_write() {
+        println!("Wants writing.");
+    }
+
+    if stream.conn.wants_read() {
+        println!("Wants reading.");
+    }
+
+    match stream.conn.complete_io(&mut stream.sock) {
+        Ok(res) => {
+            println!("Handshake done.");
+            println!("{res:?}");
+        }
+        Err(err) => {
+            println!("{err:#?}");
+            return;
+        }
+    };
+
+    if stream.conn.is_handshaking() {
+        println!("Handshaking in progress.");
+    }
+
+    if stream.conn.wants_write() {
+        println!("Wants writing.");
+    }
+
+    if stream.conn.wants_read() {
+        println!("Wants reading.");
+    }
+
+    let mut buf: [u8; 2] = [0, 0];
+    stream.read(&mut buf).unwrap();
+
+    println!("{buf:#?}");
 }
 
 fn handle_connection(mut stream: TcpStream) {
